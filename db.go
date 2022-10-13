26966ee3458112e15a574e9aa258ed2ce0190437
shin5ok <shin5ok@55mp.com>
2022-10-13 18:09:36 +0900

    implement a method to list users

--- db.go
+++ db.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"time"
 
 	"cloud.google.com/go/spanner"
 	"github.com/google/uuid"
+	"google.golang.org/api/iterator"
 )
 
 type GameUserOperation interface {
 	createUser(io.Writer, *spanner.Client, userParams) error
 	updateScore(io.Writer, *spanner.Client, userParams, int64) error
+	ListUsers(io.Writer, *spanner.Client, string) ([]map[string]interface{}, error)
 }
 
 type userParams struct {
@@ -31,6 +34,7 @@ func spannerNewClient(dbString string) (*spanner.Client, error) {
 	return client, nil
 }
 
+// create a user while initializing score field
 func (d dbClient) createUser(w io.Writer, client *spanner.Client, u userParams) error {
 
 	ctx := context.Background()
@@ -72,6 +76,7 @@ func (d dbClient) createUser(w io.Writer, client *spanner.Client, u userParams) 
 	return err
 }
 
+// update score field corresponding to specified user
 func (d dbClient) updateScore(w io.Writer, client *spanner.Client, u userParams, score int64) error {
 	ctx := context.Background()
 	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
@@ -95,4 +100,43 @@ func (d dbClient) updateScore(w io.Writer, client *spanner.Client, u userParams,
 		return nil
 	})
 	return err
+}
+
+func (d dbClient) ListUsers(w io.Writer, client *spanner.Client, name string) ([]map[string]interface{}, error) {
+	txn := client.ReadOnlyTransaction()
+	defer txn.Close()
+	// SELECT users.user_id,users.name from users join scores on users.user_id = scores.user_id where users.name = 'momo';
+	sql := "SELECT users.user_id,users.name,scores.score from users join scores on users.user_id = scores.user_id where users.name like @name;"
+	stmt := spanner.Statement{
+		SQL: sql,
+		Params: map[string]interface{}{
+			"name": fmt.Sprintf("%s%%", name),
+		},
+	}
+
+	ctx := context.Background()
+	iter := txn.Query(ctx, stmt)
+	defer iter.Stop()
+
+	var results []map[string]interface{}
+	for {
+		row, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return results, err
+		}
+		var userName string
+		var userId string
+		var userScore int64
+		if err := row.Columns(&userName, &userId, &userScore); err != nil {
+			return results, err
+		}
+
+		results = append(results, map[string]interface{}{"name": userName, "id": userId, "score": userScore})
+
+	}
+
+	return results, nil
 }
