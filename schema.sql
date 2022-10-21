68415a8b1d7f2003fb24cf51f7efbaaa4ef126b4
shin5ok <shin5ok@55mp.com>
2022-10-21 18:49:53 +0900

    get rid of white spaces no needed

--- schema.sql
+++ schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE users (             
-  user_id STRING(36) NOT NULL,   
-  name STRING(MAX) NOT NULL,     
-  created_at TIMESTAMP NOT NULL, 
-  updated_at TIMESTAMP NOT NULL, 
+CREATE TABLE users (
+  user_id STRING(36) NOT NULL,
+  name STRING(MAX) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
 ) PRIMARY KEY(user_id);
 
-CREATE TABLE user_items (                          
-  user_id STRING(36) NOT NULL,                 
-  item_id STRING(36) NOT NULL,                
-  created_at TIMESTAMP NOT NULL,               
-  updated_at TIMESTAMP NOT NULL,               
-) PRIMARY KEY(user_id, item_id),              
+CREATE TABLE user_items (
+  user_id STRING(36) NOT NULL,
+  item_id STRING(36) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+) PRIMARY KEY(user_id, item_id),
   INTERLEAVE IN PARENT users ON DELETE CASCADE;
 
 CREATE TABLE items (
   item_id STRING(36) NOT NULL,
   item_name STRING(64) NOT NULL,
   price INT64 NOT NULL,
-  created_at TIMESTAMP NOT NULL,               
-  updated_at TIMESTAMP NOT NULL,               
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
 ) PRIMARY KEY(item_id);
