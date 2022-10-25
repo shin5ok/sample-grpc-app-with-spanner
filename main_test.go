e9700282751f0d23f7fd48f7f2bb2152f77dc935
shin5ok <shin5ok@55mp.com>
2022-10-25 15:55:40 +0900

    seperate common part from test codes

--- main_test.go
+++ main_test.go
@@ -12,27 +12,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_run(t *testing.T) {
-
-	ctx := context.Background()
-
-	srv, err := spannertest.NewServer("localhost:0")
-	assert.Nil(t, err)
-	os.Setenv("SPANNER_EMULATOR_HOST", srv.Addr)
-	os.Setenv("PORT", "12820")
-	fakeDbString := "projects/your-project-id/instances/foo/databases/bar"
-	client, err := spanner.NewClient(ctx, fakeDbString)
-	assert.Nil(t, err)
-
-	s := Serving{
+var (
+	client       *spanner.Client
+	fakeDbString = "projects/your-project-id/instances/foo-instance/databases/bar"
+	fakeServing  = Serving{
 		Client: dbClient{sc: client},
 	}
+)
+
+func init() {
+	srv, _ := spannertest.NewServer("localhost:0")
+	// assert.Nil(t, err)
+	os.Setenv("SPANNER_EMULATOR_HOST", srv.Addr)
+	os.Setenv("PORT", "12820")
+	ctx := context.Background()
+
+	client, _ = spanner.NewClient(ctx, fakeDbString)
+
+}
+
+func Test_run(t *testing.T) {
 
 	req, err := http.NewRequest("GET", "/", nil)
 	assert.Nil(t, err)
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(s.pingPong)
+	handler := http.HandlerFunc(fakeServing.pingPong)
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
