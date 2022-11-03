d5aa72cb65f712cf53c7a8526f71ac412b10b692
shin5ok <shin5ok@55mp.com>
2022-11-03 14:07:52 +0900

    just modify

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 
 	r.Handle("/metrics", promhttp.Handler())
 
-	r.Get("/test", s.pingPong)
+	r.Get("/ping", s.pingPong)
 
 	r.Get("/api/users/{user_id:[a-z0-9-.]+}", s.getUserItems)
 
@@ -120,5 +120,5 @@ func (s Serving) addItemToUser(w http.ResponseWriter, r *http.Request) {
 
 func (s Serving) pingPong(w http.ResponseWriter, r *http.Request) {
 	render.Status(r, http.StatusOK)
-	render.JSON(w, r, map[string]string{"Ping": "Pong"})
+	render.PlainText(w, r, "Pong\n")
 }
